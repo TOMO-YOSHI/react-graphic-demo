b699892b8f20580b161fa836615ce00f1567a738
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-09-20 13:30:35 -0700

    Updates

---
+++ precache-manifest.f289f728afd142a5cf8550f8f03042dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7f3718d4139b16ceced576f93578b7",
+    "url": "/react-graphic-demo/index.html"
+  },
+  {
+    "revision": "1b410416e0b477f31a80",
+    "url": "/react-graphic-demo/static/css/main.f4513110.chunk.css"
+  },
+  {
+    "revision": "cad008b1246dfba0ad53",
+    "url": "/react-graphic-demo/static/js/2.6357bbbc.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/react-graphic-demo/static/js/2.6357bbbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b410416e0b477f31a80",
+    "url": "/react-graphic-demo/static/js/main.5af20186.chunk.js"
+  },
+  {
+    "revision": "163260635c02b7b2d9f4",
+    "url": "/react-graphic-demo/static/js/runtime-main.98dd8618.js"
+  }
+]);
